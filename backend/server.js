96fa32fb8189f536508553eade426e34800fe844
Ezequiel <ezequielsotoseoane@gmail.com>
2025-03-11 20:20:27 +0100

    servidor con ids

--- backend/server.js
+++ backend/server.js
@@ -39,43 +39,48 @@ app.get("/getRecetas", getRecetas);
 //Rutas post
 app.post("/authPWD", autorizacionPWD);
 app.post("/authToken", autorizacionToken);
-app.post("/create-act", crearCuenta);
+app.post("/createActMedico", crearCuentaMedico);
+app.post("/createAct", crearCuenta);
+app.post("/insertarRecetas", crearCuenta);
 
 //-------------  FUNCIONES  -------------
 //Funciones get
 function getRecetas(request, response) {
-  let tokenLogin=document.body.tokenLogin
-  if(tokenLogin){
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, token_data) {
-      if (err) {
+  let tokenLogin = document.body.tokenLogin;
+  if (tokenLogin) {
+    jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET,
+      function (err, token_data) {
+        if (err) {
           response.json({
-            correcto: 0
-          })
-      } else {
-        jsonRespuesta = {
-          correcto:1,
-          recetas: [
-            {
-              nombre: "Paracetamol",
-              inicio: "16/03/2025",
-              fin: "21/03/2025",
-              dosis: 7,
-              intervalos: 8,
-            },
-            {
-              nombre: "Ibuprofeno",
-              inicio: "12/03/2025",
-              fin: "17/03/2025",
-              dosis: 4,
-              intervalos: 12,
-            },
-          ],
-        };
-        response.json(jsonRespuesta);
-      }})
+            correcto: 0,
+          });
+        } else {
+          jsonRespuesta = {
+            correcto: 1,
+            recetas: [
+              {
+                nombre: "Paracetamol",
+                inicio: "16/03/2025",
+                fin: "21/03/2025",
+                dosis: 7,
+                intervalos: 8,
+              },
+              {
+                nombre: "Ibuprofeno",
+                inicio: "12/03/2025",
+                fin: "17/03/2025",
+                dosis: 4,
+                intervalos: 12,
+              },
+            ],
+          };
+          response.json(jsonRespuesta);
+        }
+      }
+    );
   }
-  
-  
 }
 //Funciones post
 function autorizacionPWD(request, response) {
@@ -95,11 +100,12 @@ function autorizacionPWD(request, response) {
         } else {
           if (results.length > 0) {
             if (await bcrypt.compare(passwd, results[0].passwd)) {
-              let tokenLogin = generateAccessToken(dni)
+              let tokenLogin = generateAccessToken(dni);
               response.json({
                 correcto: 1,
-                "tokenLogin": tokenLogin
-            });
+                tokenLogin: tokenLogin,
+                id:results[0].id
+              });
             }
           }
         }
@@ -116,8 +122,8 @@ function autorizacionPWD(request, response) {
 }
 
 function autorizacionToken(request, response) {
-  let tokenLogin=document.body.tokenLogin
-  if(tokenLogin){
+  let tokenLogin = document.body.tokenLogin;
+  if (tokenLogin) {
     conexion.query(
       "SELECT * FROM CodigoQR WHERE token = ? ",
       [tokenLogin],
@@ -141,29 +147,64 @@ function autorizacionToken(request, response) {
                     correcto: 0,
                     mensaje: error.message,
                   });
-                }else{
+                } else {
                   if (results.length > 0) {
-                  dni=results[0].dni
-                  let tokenLogin = generateAccessToken(dni)
-                  response.json({
-                    correcto: 1,
-                    "tokenLogin": tokenLogin
-                  });
+                    dni = results[0].dni;
+                    let tokenLogin = generateAccessToken(dni);
+                    response.json({
+                      correcto: 1,
+                      tokenLogin: tokenLogin,
+                      id:results[0].id
+                    });
+                  }
                 }
-                } 
-              })
-            
+              }
+            );
           }
         }
       }
     );
-
   }
-
-
 }
 
 async function crearCuenta(request, response) {
+  dni = request.body.dni;
+  passwd = request.body.passwd;
+  edad = request.body.edad;
+  nombre = request.body.nombre;
+  apellido1 = request.body.apellido1;
+  apellido2 = request.body.apellido2;
+  num_tlf = request.body.num_tlf;
+  let encryptedPasswd = await bcrypt.hash(passwd, saltRounds);
+
+  if (dni && passwd && edad && nombre && apellido1 && apellido2 && num_tlf) {
+    conexion.query(
+      "INSERT into Paciente (dni,edad,nombre,apellido1,apellido2,passwd,num_tlf) values(?,?,?,?,?,?,?)",
+      [dni, edad, nombre, apellido1, apellido2, encryptedPasswd, num_tlf],
+      async function (error) {
+        if (error) {
+          response.json({
+            correcto: 0,
+            mensaje: error.message,
+          });
+        } else {
+          response.json({
+            correcto: 1,
+            mensaje: "Cuenta creada",
+            token: token,
+          });
+        }
+      }
+    );
+  } else {
+    response.json({
+      correcto: 0,
+      mensaje: "Faltan campos",
+    });
+  }
+}
+
+async function crearCuentaMedico(request, response) {
   dni = request.body.dni;
   passwd = request.body.passwd;
   edad = request.body.edad;
@@ -200,8 +241,8 @@ async function crearCuenta(request, response) {
                   id = results[0].id;
                   token = maketoken(10);
                   conexion.query(
-                    "INSERT into CodigoQR (token,paciente) values(?,?)",
-                    [token, id],
+                    "INSERT into CodigoQR (token,paciente,uso) values(?,?,?)",
+                    [token, id,'login'],
                     async function (error) {
                       if (error) {
                         console.log(error);
@@ -232,6 +273,8 @@ async function crearCuenta(request, response) {
     });
   }
 }
+
+
 
 // app.del('/', function(req, res) {
 //   res.json({ mensaje: 'Método delete' })
