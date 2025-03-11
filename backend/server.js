2b9dfd729a0c674e3faabd71ea4384c4f5e05a78
Ezequiel <ezequielsotoseoane@gmail.com>
2025-03-11 11:56:55 +0100

    Prueba servidor con ejemplos calendario

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,47 @@
-var express = require('express') //llamamos a Express
-var app = express()               
+//Importación de paquetes
+var express = require('express')
+var app = express()
+const mysql = require("mysql");               
+require("dotenv").config();
 
-var port = process.env.PORT || 8080  // establecemos nuestro puerto
 
-app.get('/', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })   
-})
+//Puerto del servidor
+var port = process.env.PORT || 8080  
 
-app.get('/test', function(req, res) {
-  res.json({ mensaje: '¡test!' })  
-})
 
-app.post('/', function(req, res) {
-  res.json({ mensaje: 'Método post' })   
-})
 
-app.del('/', function(req, res) {
-  res.json({ mensaje: 'Método delete' })  
-})
+//-------------  RUTAS  -------------
+
+//Rutas get
+app.get('/calendario', calendario)
+
+//Rutas post
+app.post('/auth', autorizacion)
+
+
+
+
+//-------------  FUNCIONES  -------------
+//Funciones get
+function calendario(request,response){
+  jsonRespuesta = {
+    recetas:[
+      {nombre:'Paracetamol',inicio:'16/03/2025',fin:'21/03/2025',dosis:7,intervalos:8},
+      {nombre:'Ibuprofeno',inicio:'12/03/2025',fin:'17/03/2025',dosis:4,intervalos:12}
+    ]
+
+  }
+  response.json(jsonRespuesta) 
+}
+//Funciones post
+function autorizacion(request, response) {
+  response.json({ mensaje: 'Método post' })   
+}
+
+
+// app.del('/', function(req, res) {
+//   res.json({ mensaje: 'Método delete' })  
+// })
 
 // iniciamos nuestro servidor
 app.listen(port)
